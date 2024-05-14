6f79634149350b5cd433eea50cc4702471faf272
Ahmad Sardar <139366663+AhmadSardar402@users.noreply.github.com>
2024-05-14 14:28:41 +0500

    Error

--- ReduxToolkitPractice/src/components/Products.jsx
+++ ReduxToolkitPractice/src/components/Products.jsx
@@ -1,35 +1,33 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Card, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import {add} from './store/cartSlice'
+import { add } from './store/cartSlice'
 import { getProducts } from './store/productSlice'
+
 
 
 const Products = () => {
 
-    const dispath = useDispatch();
+    const dispatch = useDispatch();
     const {data: products} = useSelector(state => state.products)
-
+    // const {products, setProducts} = useState([])
     useEffect(()=>{
         //api
         // fetch('https://fakestoreapi.com/products')
         // .then(data => data.json())
         // .then(result => setProducts(result))
 
-        
         // Dispatch an action to fetch products
-        dispath(getProducts());
-
+        dispatch(getProducts());
 
     },[]);
 
     const addToCart = (product) => {
         // dispatch an add action 
-        dispath(add(product))
+        dispatch(add(product))
     }
-
-    const cards = products.map(product => (
+        const cards = products.map(product => (
         <div className='col' style = {{marginBottom: '5px'}}>
             <Card key={product.id} style={{ width: '18rem'}} className='h-100'>
                 <div className="text-center">
@@ -47,7 +45,7 @@ const Products = () => {
 
             </Card>
         </div>
-    ))
+    ));
   return (
     <>
         <div className="row">
